620b101f8662b4991a1f37241e5d37a6130eed47
Priyansu <prriiyansunegi@gmail.com>
2024-12-24 02:20:49 +0530

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -5,7 +5,7 @@
  
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
- import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+ import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
  import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,6 +21,9 @@ const firebaseConfig = {
 };
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+auth.languageCode = 'en'
+const provider = new GoogleAuthProvider();
 
  function showMessage(message, divId){
     var messageDiv=document.getElementById(divId);
