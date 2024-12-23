de4d8ce25ba257d7210db21b989ea981ecd50a40
Priyansu <prriiyansunegi@gmail.com>
2024-12-24 01:30:47 +0530

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,13 +1,22 @@
  // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+ 
  import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
  import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
  
- const firebaseConfig = {
- //YOUR COPIED FIREBASE PART SHOULD BE HERE
- //WATCH THIS VIDEO TO LEARN WHAT TO PUT HERE   https://youtu.be/_Xczf06n6x0
- };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7ipgfhUWHKHXi6EvmEBpYkYrtUqv4d8Q",
+  authDomain: "login-form-4d403.firebaseapp.com",
+  projectId: "login-form-4d403",
+  storageBucket: "login-form-4d403.firebasestorage.app",
+  messagingSenderId: "257470643649",
+  appId: "1:257470643649:web:4e6956b378da38b054c340"
+};
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
 
